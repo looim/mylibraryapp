ee5e99cb84e7359adfc0101ad2f44d31cf6e779f
looim <your_email@github.com>
2021-05-13 12:25:16 +0800

    now connecting to mongo

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -5,32 +5,36 @@ const Author = mongoose.model("Author")
 
 // get all authors
 const getAllAuthors = async (req, res) => {
-    try {
-        const authors = await Author.find() 
-        return res.send(authors)
-    } catch (err) {
-        res.status(400) 
-        return res.send("Database query failed")
-    } 
+  try {
+    const authors = await Author.find()
+    return res.send(authors)
+  } catch (err) {
+    res.status(400)
+    return res.send("Database query failed")
+  }
 }
 
 // find one author by their id
-const getOneAuthor = async (req, res) => {
+const getOneAuthor = async (req, res) => {  
     try {
-        const oneAuthor = await Author.findOne( {"authorId": req.params.id})
-        if (oneAuthor === null) { // no author found in database 
+        const oneAuthor = await Author.findOne( {"authorId": req.params.authorId} )
+        if (oneAuthor === null) {   // no author found in database
             res.status(404)
-            return res.send("Author not found") 
+            return res.send("Author not found")
         }
-        return res.send(oneAuthor) // author was found 
+        return res.send(oneAuthor)  // author was found
+    } catch (err) {     // error occurred
+        res.status(400)
+        return res.send("Database query failed")
+    }
+}
 
-        } catch (err) { // error occurred
-            res.status(400)
-            return res.send("Database query failed") }
-        }
-        
-// remember to export the functions 
+// change an author (POST)
+
+// add an author (POST)
+
+
+// remember to export the functions
 module.exports = {
-  getAllAuthors,
-  getOneAuthor
+  getAllAuthors, getOneAuthor //, updateAuthor, addAuthor
 }
